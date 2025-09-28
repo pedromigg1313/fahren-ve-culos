369febf15c5e36a119163a9502c7251153dd1a75
pedromigg1313 <00001105940330sp@al.educacao.sp.gov.br>
2025-09-28 18:47:35 -0300

    Add files via upload

---
+++ config_sobre.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sobre</title>
+    <link rel="icon" type="image/png" href="img/logo-oficial.png">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+<body>
+    <div class="main-container">
+
+        <section class="container text-light main-section">
+            <h1 class="display-1 text-center fw-bolder">O Que é Fahren Veículos?</h1>
+            <article>
+                <p class="mx-5 mb-4 text-center">
+                    <b>Fahren Veículos é o e-commerce mais inovador de 2025</b>, criado especialmente para você que está lendo! Nosso compromisso é entregar uma <b>experiência excepcional em cada interação</b>. Nossa missão vai além da venda de veículos - queremos revolucionar a forma como as pessoas compram carros pela internet. Com tecnologia de ponta, interface intuitiva e um atendimento personalizado, garantimos praticidade e segurança em todo o processo. Oferecemos condições exclusivas, diversas opções de pagamento, entrega ágil e uma <b>plataforma 100% digital</b>.<br>
+                    <b>Valorizamos cada cliente como único e trabalhamos para superar expectativas</b>. Na Fahren, você não está apenas comprando um veículo - está adquirindo uma experiência diferenciada que prioriza sua satisfação e confiança em <b>primeiro lugar</b>.
+                </p>
+            </article>
+        </section>
+        <div class="row justify-content-center g-4 image-section">
+            <div class="col-auto">
+                <img src="https://cdn.borainvestir.b3.com.br/2024/12/18150205/quanto-e-onde-investir-para-comprar-um-carro-zero-em-2025.webp" alt="Descrição da imagem 1" id="img_um" class="rounded object-fit-cover image-style">
+            </div>
+            <div class="col-auto">
+                <img src="https://classic.exame.com/wp-content/uploads/2022/06/capa-3.jpg" alt="Descrição da imagem 2" id="img_dois" class="rounded object-fit-cover image-style">
+            </div>
+            <div class="col-auto">
+                <img src="https://einvestidor.estadao.com.br/wp-content/uploads/2021/06/aluguelautomoveis2_140620215704.jpg" alt="Descrição da imagem 3" id="img_tres" class="rounded object-fit-cover image-style">
+            </div>
+        </div>
+        <a href="#proxima-secao" class="text-light text-decoration-none d-block saber-mais">Quer saber mais? Arraste para baixo!&nbsp;&nbsp;<i class="bi bi-arrow-down-circle-fill fs-2"></i></a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
